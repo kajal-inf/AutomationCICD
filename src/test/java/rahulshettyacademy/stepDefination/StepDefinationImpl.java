9fd43f1bf154af4a8d4486d9c9b921ca957c9819
kajal <thekajalrajput@gmail.com>
2026-05-15 15:44:12 +0530

    changed

--- src/test/java/rahulshettyacademy/stepDefination/StepDefinationImpl.java
+++ src/test/java/rahulshettyacademy/stepDefination/StepDefinationImpl.java
@@ -1,18 +1,18 @@
-package rahulshettyacademy.stepDefination;
-
-import java.io.IOException;
-
-import io.cucumber.java.en.Given;
-import rahulshettyacademy.TestComponents.BaseTest;
-import rahulshettyacademy.pageobjects.LandingPage;
-
-public class StepDefinationImpl extends BaseTest {
-	public LandingPage landingPage;
-
-	@Given("i landed on Ecommerce page")
-	public void i_landed_on_Ecommerce_page() throws IOException
-	{
-		landingPage=launchApp();
-	}
-	@Given("^logged in with username(.+)and password(.+)$")
-}
+//package rahulshettyacademy.stepDefination;
+//
+//import java.io.IOException;
+//
+//import io.cucumber.java.en.Given;
+//import rahulshettyacademy.TestComponents.BaseTest;
+//import rahulshettyacademy.pageobjects.LandingPage;
+//
+//public class StepDefinationImpl extends BaseTest {
+//	public LandingPage landingPage;
+//
+//	@Given("i landed on Ecommerce page")
+//	public void i_landed_on_Ecommerce_page() throws IOException
+//	{
+//		landingPage=launchApp();
+//	}
+//	@Given("^logged in with username(.+)and password(.+)$")
+//}
